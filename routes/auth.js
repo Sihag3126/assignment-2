a398b7edbc3ab8ee08f9c213b97a9fc89533b6fa
Sihag3126 <sihag3126@gmail.com>
2024-08-07 15:36:26 -0400

    Add explanatory comments on routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Route to display the registration page
 router.get('/register', authController.registerPage);
+
+// Route to handle user registration form submission
 router.post('/register', authController.register);
+
+// Route to display the login page
 router.get('/login', authController.loginPage);
+
+// Route to handle user login form submission
 router.post('/login', authController.login);
+
+// Route to handle user logout
 router.get('/logout', authController.logout);
+
 module.exports = router;
